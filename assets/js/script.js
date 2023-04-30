69241a56f62ae92bf753271cdfab782d48b7eeef
elliotjonesdev <elliotjonesdev@gmail.com>
2023-04-30 19:12:02 +0000

    add get commands for game elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,5 +27,54 @@ function startGame() {
   function incrementWrongAnswer() {
 
   }
+
+  const questions = [
+    {
+      question: "What is the 400m mens world record?",
+      answers: [
+        { text: "41.67s", correct: false },
+        { text: "45.01s", correct: false },
+        { text: "43.03s", correct: true },
+        { text: "49.77s", correct: false }
+      ]
+    },
+    {
+      question: "What is the biggest grossing movie?",
+      answers: [
+        { text: "Lion King", correct: false },
+        { text: "Mr Titanic", correct: false },
+        { text: "Avengers End Game", correct: false },
+        { text: "Avatar", correct: true }
+      ]
+    },
+    {
+      question: "What is a Juicy Salif?",
+      answers: [
+        { text: "Lemon juicer", correct: true },
+        { text: "Orange", correct: false },
+        { text: "Cocktail", correct: false },
+        { text: "Dessert", correct: false }
+      ]
+    },
+    {
+      question: "When was the first Harry Potter movie released?",
+      answers: [
+        { text: "2000", correct: false },
+        { text: "2001", correct: true },
+        { text: "2003", correct: false },
+        { text: "2004", correct: false }
+      ]
+    },
+    {
+      question: "What is an egret?",
+      answers: [
+        { text: "Car", correct: false },
+        { text: "Boat", correct: false },
+        { text: "Plane", correct: false },
+        { text: "Bird", correct: true }
+      ]
+    }
+  ];
+  
   
 
